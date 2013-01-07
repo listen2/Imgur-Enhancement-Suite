d22e4c8e7f32d74dca321a9d0fa2d90737527f63
listen2 <getifaddrs@gmail.com>
2013-01-07 16:28:55 -0500

    Update version number.

--- imgur_enhancement_suite.user.js
+++ imgur_enhancement_suite.user.js
@@ -2,13 +2,13 @@
 // @name 			Imgur Enhancement Suite
 // @namespace		imgur_listen2
 // @downloadURL	https://raw.github.com/listen2/Imgur-Enhancement-Suite/master/imgur_enhancement_suite.user.js
-// @version			1.0
+// @version			2.0.0
 // @description 	Makes a few things a little bit better.
 // @include			http://imgur.com/*
 // ==/UserScript==
 
 (function(){
-	var version = "1.0.0";
+	var version = "2.0.0";
 
 	function check_version() {
 		version_info = JSON.parse(localStorage["version_info"] || "{}");
